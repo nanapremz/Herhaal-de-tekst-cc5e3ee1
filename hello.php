bcfd1112ea350805e768510486f266af71d3230a
nanapremz <54808742+nanapremz@users.noreply.github.com>
2019-10-10 18:56:54 +0200

    Uitwerking van de exercise

---
+++ hello.php
@@ -0,0 +1,17 @@
+<?php
+$a = "Hello World!";
+$b = "Ik ben Nana Prempeh";
+$c = "Ik hou van coding";
+$x = "Ik ben 19 jaar oud";
+$y = "In de toekomst wil ik een computer scientist worden";
+
+echo $a;
+echo "<br>";
+echo $b;
+echo "<br>";
+echo $c;
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y;
+?>
